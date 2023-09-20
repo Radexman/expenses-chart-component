d6608e6d10e12b114dc9590fc03e87657a87d3f1
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-09-20 16:25:04 +0200

    Refactor chart bar to single component & type checking

--- src/components/BarChart.jsx
+++ src/components/BarChart.jsx
@@ -1,23 +1,25 @@
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import SingleBar from './SingleBar';
 
 const BarChart = ({ statistics }) => {
+  const [today, setToday] = useState();
+
+  useEffect(() => {
+    setCurrentDate();
+  }, []);
+
+  const setCurrentDate = () => {
+    const today = new Date().getDay();
+    setToday(today);
+  };
+
   return (
     <div className="space-y-8">
       <h1 className="text-2xl font-bold">Spending - Last 7 days</h1>
       <div className="flex items-end justify-evenly">
-        {statistics.map((stat, index) => {
-          const { day, amount, height } = stat;
-          return (
-            <div key={index}>
-              <div
-                className="bg-primary-softRed tooltip w-8 cursor-pointer rounded-sm md:w-14"
-                style={{ height: height }}
-                data-tip={`$${amount}`}
-              ></div>
-              <p className="text-center">{day}</p>
-            </div>
-          );
-        })}
+        {statistics.map((stat) => (
+          <SingleBar key={stat.day} stat={stat} today={today} />
+        ))}
       </div>
     </div>
   );
---
+++ src/components/SingleBar.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+
+const SingleBar = ({ stat, today }) => {
+  const { dayName, day, amount, height } = stat;
+
+  return (
+    <div>
+      <div
+        className="tooltip w-8 cursor-pointer rounded-sm md:w-14"
+        style={{
+          height: height,
+          backgroundColor:
+            today === day ? 'hsl(186, 34%, 60%)' : 'hsl(10, 79%, 65%)',
+        }}
+        data-tip={`$${amount}`}
+      ></div>
+      <p className="text-center">{dayName}</p>
+    </div>
+  );
+};
+
+SingleBar.propTypes = {
+  stat: PropTypes.shape({
+    dayName: PropTypes.string.isRequired,
+    day: PropTypes.number.isRequired,
+    amount: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+  }),
+  today: PropTypes.number.isRequired,
+};
+
+export default SingleBar;
